460bec21e1193f4d379accd4aae02f55e0ac13b2
Andre <n10470034@qut.edu.au>
2021-05-01 20:37:52 +1000

    Added descriptions for the orgUnit class and its method.

--- src/org/orgUnit.java
+++ src/org/orgUnit.java
@@ -1,39 +1,76 @@
 package org;
 
+/**
+ * A class for creating organisation unit as object
+ */
 public class orgUnit implements org{
 
     private Double orgCredit;
     private String orgName;
     private Integer orgID;
 
+    /**
+     * Create an instance of orgUnit
+     *
+     * @param orgID ID of the organisation
+     * @param orgName Name of the organisation
+     * @param orgCredit Credit that the organisation have
+     */
     public orgUnit(Integer orgID, String orgName, Double orgCredit) {
         this.orgID = orgID;
         this.orgName = orgName;
         this.orgCredit = orgCredit;
     }
 
+    /**
+     * Adds amount to the credit that the organisation owns
+     *
+     * @param amount that will be added to the current credit
+     * @return credit after adding amount
+     */
     @Override
     public Double addCredit(Double amount) {
         orgCredit = orgCredit + amount;
         return orgCredit;
     }
 
+    /**
+     * Subtract amount to the credit that the organisation owns
+     *
+     * @param amount that will be subtracted to the current credit
+     * @return credit after subtracting amount
+     */
     @Override
     public Double removeCredit(Double amount) {
         orgCredit = orgCredit - amount;
         return orgCredit;
     }
 
+    /**
+     * Get method for the credit that the organisation owns
+     *
+     * @return credit of the organisation
+     */
     @Override
     public Double orgCredit() {
         return orgCredit;
     }
 
+    /**
+     * Get method for the name of the organisation
+     *
+     * @return name of the organisation
+     */
     @Override
     public String orgName() {
         return orgName;
     }
 
+    /**
+     * Get method for the ID of the organisation
+     *
+     * @return ID of the organisation
+     */
     @Override
     public Integer orgID() {
         return orgID;
