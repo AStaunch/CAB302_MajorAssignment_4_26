06885649740a8c36e4d159f15ec8b85a1918b9a8
Andre <n10470034@qut.edu.au>
2021-05-01 20:40:26 +1000

    Added descriptions for the assetUnit class and its method.

--- src/asset/assetUnit.java
+++ src/asset/assetUnit.java
@@ -1,21 +1,41 @@
 package asset;
 
+/**
+ * A class for creating asset unit object
+ */
 public class assetUnit implements asset{
     private String assetName;
     private Integer assetID;
 
+    /**
+     * Create an instance of assetUnit
+     *
+     * @param assetName Name of the asset
+     * @param assetID ID of the asset
+     */
     public assetUnit(String assetName, Integer assetID){
         this.assetName = assetName;
         this.assetID = assetID;
 
     }
+
+    /**
+     * Get method for the name of the asset
+     *
+     * @return name of the asset
+     */
     @Override
     public String assetName() {
-        return null;
+        return assetName;
     }
 
+    /**
+     * Get method for the ID of the asset
+     *
+     * @return ID of the asset
+     */
     @Override
     public Integer assetID() {
-        return null;
+        return assetID;
     }
 }
