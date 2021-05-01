8662b21e75ced01242ad6a6f26778d777685b056
Andre <n10470034@qut.edu.au>
2021-05-01 20:19:21 +1000

    Added description for the adminUser class and its methods.

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args){
-        adminUser user = new adminUser("Testing", 6, true);
-        System.out.print(user.userID()+"\n");
 
     }
 }
--- src/user/adminUser.java
+++ src/user/adminUser.java
@@ -1,7 +1,7 @@
 package user;
 
 /**
- * A Class for Admin Users
+ * A Class for creating admin user as objects
  */
 
 public class adminUser implements user{
@@ -9,23 +9,42 @@ public class adminUser implements user{
     private Integer userID;
     private Boolean status;
 
-    public adminUser(String name, Integer userID, Boolean status){
+    /**
+     *
+     * @param name Name of the user
+     * @param userID ID of the user
+     */
+    public adminUser(String name, Integer userID){
         this.name = name;
         this.userID = userID;
-        this.status = status;
+        this.status = false;
     }
 
-
+    /**
+     * Get method for the ID of the user
+     *
+     * @return the ID of the user
+     */
     @Override
     public Integer userID() {
         return userID;
     }
 
+    /**
+     * Get method for the status of the user
+     *
+     * @return the status of the user
+     */
     @Override
     public Boolean status() {
         return status;
     }
 
+    /**
+     * Get method for the name of the user
+     *
+     * @return the name of the user
+     */
     @Override
     public String identity() {
         return name;
