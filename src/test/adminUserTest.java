343e46dc1cdfc1e8623712ca1105f2403282a366
Andre <n10470034@qut.edu.au>
2021-05-17 20:16:56 +1000

    Created Unit Testing for classes

---
+++ src/test/adminUserTest.java
@@ -0,0 +1,44 @@
+package test;
+import client.adminUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class adminUserTest {
+    adminUser user1;
+
+    @BeforeEach
+    public void initAdmin(){
+        user1 = new adminUser("Andre", 1);
+    }
+
+    @Test
+    public void testAdminName(){
+        assertEquals("Andre", user1.identity());
+    }
+
+    @Test
+    public void testAdminID(){
+        assertEquals(1, user1.userID());
+    }
+
+    @Test
+    public void testIsAdmin(){
+        assertEquals(true, user1.isAdmin());
+    }
+
+    @Test
+    public void testTwoAdmin(){
+        adminUser u1 = new adminUser("Jayden",2);
+        adminUser u2 = new adminUser("Alex", 3);
+
+        assertEquals("Jayden", u1.identity());
+        assertEquals("Alex", u2.identity());
+
+        assertEquals(2, u1.userID());
+        assertEquals(3, u2.userID());
+
+        assertEquals(true, u1.isAdmin());
+        assertEquals(true, u2.isAdmin());
+    }
+}
---
+++ src/test/assetUnitTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import common.assetUnit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class assetUnitTest {
+    assetUnit asset1;
+
+    @BeforeEach
+    public void initAsset(){
+        asset1 = new assetUnit("CPU", 1, 100.00);
+    }
+
+    @Test
+    public void testAssetName(){
+        assertEquals("CPU", asset1.assetName());
+    }
+
+    @Test
+    public void testAssetID(){
+        assertEquals(1, asset1.assetID());
+    }
+
+    @Test
+    public void testAssetPrice(){
+        assertEquals(100.00, asset1.assetPrice());
+    }
+
+    @Test
+    public void testTwoAsset(){
+        assetUnit a1 = new assetUnit("Monitor", 2, 300.00);
+        assetUnit a2 = new assetUnit("Graphics Card", 3, 500.00);
+
+        assertEquals("Monitor", a1.assetName());
+        assertEquals("Graphics Card", a2.assetName());
+
+        assertEquals(2, a1.assetID());
+        assertEquals(3, a2.assetID());
+
+        assertEquals(300.00, a1.assetPrice());
+        assertEquals(500.00, a2.assetPrice());
+    }
+}
