4ccea3d111559bb3c6c0c81189dbd6b8a4a084fb
Andre <n10470034@qut.edu.au>
2021-05-17 20:38:09 +1000

    Change variables to private

--- src/test/adminUserTest.java
+++ src/test/adminUserTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class adminUserTest {
-    adminUser user1;
+    private adminUser user1;
 
     @BeforeEach
     public void initAdmin(){
--- src/test/orgUnitTest.java
+++ src/test/orgUnitTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class orgUnitTest {
-    orgUnit org;
+    private orgUnit org;
 
     @BeforeEach
     public void initOrg(){
