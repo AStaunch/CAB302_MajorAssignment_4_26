d26cb7a0b551ab453c41154b364bfa2274dd8593
Jayden <jayden.truong@connect.qut.edu.au>
2021-05-18 19:32:48 +1000

    database test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 import client.guiClass;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException{
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cab302","root","root");
+
+
         guiClass s = new guiClass();
+
+        connection.close();
         s.runGUI();
     }
 }
