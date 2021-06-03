8a233569e8345f7d0b2ddd730f60f2114f056d2b
Andre <n10470034@qut.edu.au>
2021-06-03 13:40:05 +1000

    Added hashPassword method

--- src/client/user.java
+++ src/client/user.java
@@ -1,9 +1,14 @@
 package client;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public interface user {
     String identity();
 
     Integer userID();
 
     Boolean isAdmin();
+
+    byte[] hashPassword(String pwd) throws NoSuchAlgorithmException, InvalidKeySpecException;
 }
