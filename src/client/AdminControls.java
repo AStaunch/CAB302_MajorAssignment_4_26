1e3b9a3a13cc547df2126ac646188e0eb6c02518
Jayden <jayden.truong@connect.qut.edu.au>
2021-06-05 11:26:30 +1000

    New

--- src/client/AdminControls.java
+++ src/client/AdminControls.java
@@ -1,9 +1,6 @@
 package client;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import JDBC.DBConnection;
 
@@ -12,11 +9,13 @@ public class AdminControls {
     public static String INSERT_USER = "INSERT INTO user (org_id, username, first_name, last_name, hash_pwd," +
             "birth_date, is_admin) VALUES (?,?,?,?,?,?,?)";
     public static String REMOVE_USER = "DELETE * FROM user WHERE id=?";
-    public static String LIST_USERS = "SELECT * FROM users";
+    public static String LIST_USERS = "SELECT * FROM user";
+    public static String GET_USER = "SELECT * FROM user WHERE id=?";
 
     private PreparedStatement addUser;
     private PreparedStatement removeUser;
     private PreparedStatement listUsers;
+    private PreparedStatement getUser;
 
     public AdminControls() {
         try {
@@ -24,11 +23,31 @@ public class AdminControls {
             this.addUser = this.connection.prepareStatement(INSERT_USER);
             this.removeUser = this.connection.prepareStatement(REMOVE_USER);
             this.listUsers = this.connection.prepareStatement(LIST_USERS);
+            this.getUser = this.connection.prepareStatement(GET_USER);
 
         } catch (SQLException var2) {
             var2.printStackTrace();
         }
     }
+
+    public String getUser(int id) {
+        user u = new normalUser();
+        ResultSet rs =  null;
+
+        try{
+            this.getUser.setInt(1, id);
+            rs = this.getUser.executeQuery();
+            rs.next();
+            //u.setOrg_id(rs.getInt("org_id"));
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return "test";
+    }
+
+
 
 
     public void close() {
