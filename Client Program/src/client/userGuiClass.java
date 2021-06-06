6d55d64716d7619623107193320e33061b272d51
AStaunch <n10481320@qut.edu.au>
2021-06-06 19:24:51 +1000

    User GUI Stuff Implimented Asset Stuff

--- Client Program/src/client/userGuiClass.java
+++ Client Program/src/client/userGuiClass.java
@@ -5,6 +5,7 @@ import common.assetUnit;
 
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,8 +13,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class userGuiClass {
-    private JMenuBar mb;
-    private JMenu home;
     private final UserControls uc;
     private final AdminControls ac;
     private final normalUser user;
@@ -32,8 +31,8 @@ public class userGuiClass {
         frame.setSize(400, 200);
         frame.setLocationRelativeTo(null);
         //frame.setResizable(false);
-        home = new JMenu("User Home Menu");
-        mb = new JMenuBar();
+        JMenu home = new JMenu("User Home Menu");
+        JMenuBar mb = new JMenuBar();
         mb.add(home);
 
         frame.setJMenuBar(mb);
@@ -58,9 +57,7 @@ public class userGuiClass {
         createListingPanel.add(createListing);
         pane.add(createListingPanel);
 
-        createListing.addActionListener(e -> {
-            CreateListing(frame);
-        });
+        createListing.addActionListener(e -> CreateListing(frame));
 
         // View All Public Listings
         JPanel viewListingsPanel = new JPanel();
@@ -69,9 +66,7 @@ public class userGuiClass {
         viewListingsPanel.add(viewListings);
         pane.add(viewListingsPanel);
 
-        viewListings.addActionListener(e -> {
-            ViewAllListings(frame);
-        });
+        viewListings.addActionListener(e -> userGuiClass.this.ViewAllListings(frame));
 
         // View + edit my Current Listings
         JPanel mylistingsPanel = new JPanel();
@@ -94,7 +89,37 @@ public class userGuiClass {
         listAssets.addActionListener(e -> {
             ViewAllAssets(frame);
         });
+
+
+        // Create a listing Panel
+        JPanel editPasswordPanel = new JPanel();
+        JButton editPassword = new JButton("Create New Listing");
+        editPassword.setPreferredSize(new Dimension(150,25));
+        editPasswordPanel.add(editPassword);
+        pane.add(editPasswordPanel);
+
+        editPassword.addActionListener(e -> {
+            EditPassword(frame);
+        });
     }
+
+    private void EditPassword(JFrame previousFrame) {
+        JFrame currentFrame = new JFrame("View all Listings");
+        currentFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        currentFrame.setSize(400, 200);
+        currentFrame.setLocationRelativeTo(previousFrame);
+        EnabledOnClose(currentFrame, previousFrame);
+
+        JPanel textPanel = new JPanel(new BoxLayout(currentFrame.getContentPane(), BoxLayout.Y_AXIS ));
+        JLabel current = new JLabel("Please enter you current Password:");
+        JTextField currentPW = new JTextField("Current Password");
+        JLabel newP = new JLabel("Please enter you current Password:");
+        JTextField newPW = new JTextField("Current Password");
+        JLabel repeat = new JLabel("Please enter you current Password:");
+        JTextField repeatPW = new JTextField("Current Password");
+
+    }
+
     private void CreateListing(JFrame previousFrame){
 
         JFrame currentFrame = new JFrame("View all Listings");
@@ -102,12 +127,39 @@ public class userGuiClass {
         currentFrame.setSize(400, 200);
         currentFrame.setLocationRelativeTo(previousFrame);
         EnabledOnClose(currentFrame, previousFrame);
+        currentFrame.setVisible(true);
 
+        JPanel panel = new JPanel();
         JLabel listedAssetLabel = new JLabel("Select an Asset to Sell");
 
-        String[] myAssets = uc.a.listInvAsset();
-        JComboBox assetsAvailable = new JComboBox(myAssets);
+        String[] assetNames = uc.a.list_type().toArray(new String[0]);
+
+        JComboBox assetsAvailable = new JComboBox(assetNames);
+        JTextField qtyBox = new JTextField();
+        JTextField listBox = new JTextField();
+
+
+        JCheckBox isBuy = new JCheckBox("Is this a buy Listing?");
+        isBuy.setEnabled(false);
+        JButton done = new JButton("Create Listing!");
+        done.addActionListener(e -> {
+
+            int assetID = 1; //place holder
+
+            int qty = java.lang.Integer.parseInt(qtyBox.toString());
+            int listPrice = java.lang.Integer.parseInt(listBox.toString());
+            if(isInteger(qtyBox.toString()) && isInteger(qtyBox.toString())){
+                assetUnit asset = new assetUnit(user.getOrgID(),user.getID(), assetID, qty , listPrice, isBuy.isSelected());
+            }else{
+
+            }
+
+        });
+
+        panel.add(qtyBox);
+        currentFrame.add(panel);
     }
+
     private void ViewAllListings(JFrame previousFrame){
         JFrame currentFrame = new JFrame("View My Listings");
         currentFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -118,14 +170,18 @@ public class userGuiClass {
         JPanel tablePanel = new JPanel();
         tablePanel.setLayout(new FlowLayout());
 
-        assetUnit[] allListedAssets = new assetUnit[]{new assetUnit()};
-        allListedAssets = uc.viewListing(true).toArray(new assetUnit[0]);
+        //Array of Buy Orders
+        assetUnit[] allListedBuys = uc.viewListing(true).toArray(new assetUnit[0]);
+        //Array pf Sell Orders
+        assetUnit[] allListedSells = uc.viewListing(false).toArray(new assetUnit[0]);
+
+        assetUnit[] allListedAssets = mergeAssetArrays(allListedBuys, allListedSells);
+
         // TODO update to take in all a user/organisations listings]);
 
-        String[] columnNames = new String[]{"Asset Listed","Listed Price", "Actions"};
+        String[] columnNames = new String[]{"Asset Listed", "Listed Price", "Actions"};
 
-        JPanel table = createTable(columnNames, allListedAssets, currentFrame);
-
+        JPanel table = createAssetTable(columnNames, allListedAssets, currentFrame);
         tablePanel.add(table);
         currentFrame.add(tablePanel);
         currentFrame.setVisible(true);
@@ -144,7 +200,7 @@ public class userGuiClass {
         // TODO update to take in all a user/organisations listings
         String[] columnNames = new String[]{"Asset Listed","Listed Price"};
 
-        JPanel table = createTable(columnNames, myListedAsset, currentFrame);
+        JPanel table = createAssetTable(columnNames, myListedAsset, currentFrame);
 
         tablePanel.add(table);
         currentFrame.add(tablePanel);
@@ -152,11 +208,6 @@ public class userGuiClass {
 
 
     }
-
-    private void ViewListing(assetUnit listedAsset) {
-
-    }
-
     private void ViewAllAssets(JFrame previousFrame) {
 
         JFrame currentFrame = new JFrame("Electronic Asset Trading Platform");
@@ -169,18 +220,24 @@ public class userGuiClass {
         JPanel tablePanel = new JPanel();
         tablePanel.setLayout(new FlowLayout());
 
-        assetUnit[] assetUnits = new assetUnit[]{new assetUnit()};
+        String[] assetInfo = uc.a.listInvAsset();
+        String[][] rowData = new String[assetInfo.length][];
+        for(int i = 0; i < assetInfo.length; i++){
+            rowData[i] = assetInfo[i].split(", ", 3);
+        }
+
+
         // TODO update to take in all a user/organisations listings
         String[] columnNames = new String[]{"Asset Name","Asset Price"};
 
-        JPanel table = createTable(columnNames, assetUnits, currentFrame);
+        JTable table = new JTable(rowData, columnNames);
+        //JPanel table = createAssetTable(columnNames, assetUnits, currentFrame);
 
         tablePanel.add(new JScrollPane(table));
         tablePanel.add(table);
         currentFrame.add(tablePanel);
         currentFrame.setVisible(true);
     }
-
     private void EnabledOnClose(JFrame currentFrame,JFrame previousFrame){
         previousFrame.setEnabled(false);
 
@@ -190,44 +247,60 @@ public class userGuiClass {
             };
         });
     }
-
-    private JPanel createTable(Object[] columnNames, assetUnit[] dataArray , JFrame previousFrame){
+    private JPanel createAssetTable(Object[] columnNames, assetUnit[] dataArray , JFrame previousFrame){
 
         JPanel returnPanel = new JPanel( new FlowLayout(FlowLayout.CENTER, 0, 0) );
         JPanel tablePanel = new JPanel();
         JPanel ButtonPanel = new JPanel();
+        ButtonPanel.setLayout(new BoxLayout(ButtonPanel, BoxLayout.Y_AXIS));
         Object[][] rowData = new Object[dataArray.length][];
 
-        for (int i = 0; i < dataArray.length; i++) {
-            int itemID = dataArray[i].getID();
+        int height = 30;
+        int width = 100;
 
-            rowData[i] = new Object[]{};
+        if ((dataArray.length > 0))
+        {
+            for (int i = 0; i < dataArray.length; i++) {
 
-            JButton viewAsset = new JButton("View");
-            int finalI = i;
-            viewAsset.addActionListener(e -> {
-                ViewAsset(previousFrame, dataArray[finalI]);
-            });
-            ButtonPanel.add(viewAsset);
+                int itemID = dataArray[i].getID();
+                String itemName = uc.a.getInvAsset(dataArray[i].getAsset()).getType();
+                String itemPrice = dataArray[i].getCredits().toString();
+                String itemSeller = uc.a.getOrgByID(dataArray[i].getOrg()).getName();
 
+                rowData[i] = new String[]{itemName, itemPrice, itemSeller};
+
+                JButton viewAsset = new JButton("View");
+                int finalI = i;
+                viewAsset.addActionListener(e -> {
+                    ViewAsset(previousFrame, dataArray[finalI]);
+                });
+                viewAsset.setPreferredSize(new Dimension(width, height));
+                ButtonPanel.add(viewAsset);
+
+            }
+
+            JTable table = new JTable(rowData, columnNames);
+            table.setRowHeight(height);
+
+            TableColumnModel columnModel = table.getColumnModel();
+            for(int i = 0; i < columnNames.length; i++){
+                columnModel.getColumn(i).setWidth(width);
+            }
+
+            table.removeEditor();
+            table.setRowSelectionAllowed(false);
+            tablePanel.add(new JScrollPane(table));
+
+            tablePanel.add(table);
+            returnPanel.add(tablePanel);
+            returnPanel.add(ButtonPanel);
+
+
+        }else{
+            returnPanel.add(new JLabel("There are no Assets that fit this criteria"));
         }
-
-        JTable table = new JTable(rowData, columnNames);
-        table.removeEditor();
-
-        TableCellRenderer buttonRenderer = new ButtonRenderer();
-        table.getColumnModel().getColumn(2).setCellRenderer(new ButtonRenderer());
-        table.getColumnModel().getColumn(2).setCellEditor(new ButtonEditor(new JTextField()));
-        table.setRowSelectionAllowed(false);
-
-        tablePanel.add(table);
-        returnPanel.add(tablePanel);
-        returnPanel.add(ButtonPanel);
-
-
         return returnPanel;
     }
-
     private void ViewAsset(JFrame previousFrame, assetUnit unit){
         JFrame currentFrame = new JFrame("Electronic Asset Trading Platform");
         currentFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -238,17 +311,26 @@ public class userGuiClass {
 
         JPanel mainPanel = new JPanel(new FlowLayout());
         Container pane = currentFrame.getContentPane();
-        JPanel buyPanel = new JPanel(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        JPanel sellPanel = new JPanel(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        JPanel infoPanel = new JPanel(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        JPanel buyPanel = new JPanel();
+        buyPanel.setLayout(new BoxLayout(buyPanel, BoxLayout.Y_AXIS));
+        JPanel sellPanel = new JPanel();
+        sellPanel.setLayout(new BoxLayout(sellPanel, BoxLayout.Y_AXIS));
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
 
 
 
         //Asset Information
         String assetName = ac.getInvAsset(unit.getID()).getType();
+        int userOrgID = user.getOrgID();
         int quantityAvailable = ac.getInvAsset(unit.getID()).getQTY();
-        int quantityOwned = ac.getInvAssetTO(assetName, user.getOrgID()).getQTY();
-        int lowestPrice = 0;
+        int quantityOwned;
+        try {
+            quantityOwned = uc.a.getInvAssetTO(assetName, userOrgID).getQTY();
+        }catch (Exception e){
+            quantityOwned = 0;
+        }
+        int lowestPrice = 1;
         //TODO
 
         JLabel unitName = new JLabel(assetName);
@@ -274,11 +356,12 @@ public class userGuiClass {
                 Boolean goodPrice = true;
                 //TODO
                 if (goodQty && goodPrice){
-                    //uc.buyItem(unit, user);
+                    uc.generalBuy(unit, user, qtyBought);
                 }else if(!goodQty){
-
+                    JDialog popup = new JDialog();
+                    JOptionPane.showMessageDialog(null, "Please Enter a valid Quantity");
                 }else if(!goodPrice){
-
+                    JOptionPane.showMessageDialog(null, "Please Enter a valid Price.");
                 }
             }
         });
@@ -288,24 +371,30 @@ public class userGuiClass {
 
         //Sell Functions
         //Type, Qty, stockSold
-        JTextField qtySell = new JTextField("Please Enter a Quantity to buy");
-        JTextField priceSell = new JTextField("Please Enter a Price Per Unit to buy at");
+        JTextField qtySell = new JTextField("Please Enter a Quantity to sell");
+        JTextField priceSell = new JTextField("Please Enter a Price Per Unit to sell at");
 
-        JButton sellButton = new JButton("Buy This");
+        JButton sellButton = new JButton("Sell This");
         sellButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int qtySold = Integer.parseInt(qtySell.toString());
-                Boolean goodQty = quantityOwned >= qtySold && isInteger(qtySell.toString());
+                Boolean goodQty = isInteger(qtySell.toString());
 
-                Boolean goodPrice = true;
+                int sellPrice = Integer.parseInt(priceSell.toString());
+                Boolean goodPrice = isInteger(priceSell.toString());
+                unit.setCredits(sellPrice);
                 //TODO
                 if (goodQty && goodPrice){
-                    //uc.buyItem(unit, user);
+                    for(int i = 0; i < qtySold; i++){
+                        uc.listItem(unit);
+                    }
+
                 }else if(!goodQty){
-
+                    JDialog popup = new JDialog();
+                    JOptionPane.showMessageDialog(null, "Please Enter a valid Quantity");
                 }else if(!goodPrice){
-
+                    JOptionPane.showMessageDialog(null, "Please Enter a valid Price.");
                 }
             }
         });
@@ -314,9 +403,9 @@ public class userGuiClass {
         sellPanel.add(sellButton);
 
         //Add To Main Panel
-        mainPanel.add(infoPanel);
-        mainPanel.add(buyPanel);
-        mainPanel.add(sellPanel);
+        mainPanel.add(infoPanel, BorderLayout.NORTH);
+        mainPanel.add(buyPanel, BorderLayout.EAST);
+        mainPanel.add(sellPanel, BorderLayout.WEST);
         currentFrame.add(mainPanel);
         currentFrame.setVisible(true);
     }
@@ -343,6 +432,16 @@ public class userGuiClass {
             }
         }
         return true;
+    }
+
+    public assetUnit[] mergeAssetArrays(assetUnit[] firstArray, assetUnit[] secondArray){
+
+        int fal = firstArray.length;        //determines length of firstArray
+        int sal = secondArray.length;   //determines length of secondArray
+        assetUnit[] result = new assetUnit[fal + sal];  //resultant array of size first array and second array
+        System.arraycopy(firstArray, 0, result, 0, fal);
+        System.arraycopy(secondArray, 0, result, fal, sal);
+        return result;
     }
 }
 //BUTTON RENDERER CLASS
